f88fc8956e7a71c48dbb52ecc3253a1bafd744a9
Kyle B <kyleb44@hotmail.com>
2024-12-27 14:10:23 -0500

    add caesar-cipher

---
+++ caesar-cipher.rb
@@ -0,0 +1,31 @@
+def caesar_cipher(string, shift) 
+  letters = string.split("")
+
+  letters_shifted = letters.map do |letter|
+    value = letter.ord
+
+    if value.between?(65, 90)
+      value += shift
+      if value > 90
+        value -= 26
+      end
+      value.chr
+
+    elsif value.between?(97,122)
+      value += shift
+      if value > 122
+        value -= 26
+      end
+      value.chr
+    else
+      letter
+    end
+
+  end
+
+  puts letters_shifted.join
+end
+
+
+
+caesar_cipher("What a String!", 3)
